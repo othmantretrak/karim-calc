b89dc9566df7452ddef168d4f8c6ac9854effec9
tretrak <3trak10@gmail.com>
2025-10-03 16:41:25 +0100

    imlement the second approach2

--- src/app/dashboard/products/page.tsx
+++
@@ -1,8 +0,0 @@
-import AdminProductsPage from '@/app/components/AdminProductsPage'
-import { getAllProducts } from '@/app/actions/productActions'
-
-export default async function AdminProductsPageServer() {
-    const products = await getAllProducts()
-
-    return <AdminProductsPage products={products} />
-}
