96a307c1e4409436a16d3adbd3b2ce2072851839
tretrak <3trak10@gmail.com>
2025-10-10 02:40:11 +0100

    working - fix thankyou

--- src/components/calculator/Thankyou.tsx
+++ src/components/calculator/Thankyou.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Thankyou() {
     return (
         <div className="text-center py-12 space-y-4">
-            <h2 className="text-2xl font-bold text-green-700">Thank you!</h2>
+            <h2 className="text-2xl font-bold text-green-700">Bedankt!</h2>
             <p className="text-gray-700">
-                We have received your request and<br />
-                will get back to you within 6 hours during our<br />
-                regular working hours!
+                We hebben uw aanvraag ontvangen en<br />
+                nemen binnen 6 uur tijdens onze<br />
+                reguliere werktijden contact met u op!
             </p>
             <div className="pt-4">
-                <p className="font-medium">Kind regards</p>
+                <p className="font-medium">Met vriendelijke groet</p>
                 <p className="font-medium">GreenTeam</p>
             </div>
         </div>
